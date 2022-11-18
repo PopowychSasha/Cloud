bb9fd44828d37eb46b0a75433a65d23f841ccdfa
PopowychSasha <60547333+PopowychSasha@users.noreply.github.com>
2022-11-18 11:40:44 +0200

    do ability to use middlewares in application

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Server from '../lib/server.js'
 const app = new Server()
 
 app.get('/user', (req, res) => {
-  res.status(200).json({ message: 'This is get:user HTTP method2' })
+  res.status(200).json({ message: 'This is get:user HTTP method' })
 })
-app.post('/user', (req, res) => {
+app.post('/user', async (req, res) => {
+  const body = await req.toJson()
+  console.log(body)
   res.status(201).json({ message: 'This is post:user HTTP method' })
 })
 app.put('/user', (req, res) => {
@@ -13,7 +15,6 @@ app.put('/user', (req, res) => {
 app.delete('/user', (req, res) => {
   res.status(200).json({ message: 'This is delete:user HTTP method' })
 })
-
 app.listen(5000, (port) => {
   console.log(`Server is started on port ${port}`)
 })
