3485c5bea7be5991f3a67e2d2608f8dccfabd4e1
PopowychSasha <60547333+PopowychSasha@users.noreply.github.com>
2022-11-24 15:40:54 +0200

    create crud operation for user (#13)
    * create crud operation for user
    
    * return user when create update or delete
    
    Co-authored-by: sasha_popovych <sashapopovych2003@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,24 +1,51 @@
 import Server from '../lib/server.js'
+import UserModel from './model/user.js'
 const app = new Server()
 
-app.get('/user/:id', (req, res, next) => {
-  try {
-    console.log(req.params)
-    res.status(200).json({ message: 'This is get:user HTTP method' })
-  } catch (err) {
-    next(err)
+const user = new UserModel()
+
+app.get('/user/:id', (req, res) => {
+  const { id } = req.params
+  const entity = user.selectById(+id)
+
+  if (!entity) {
+    return res.status(404).json({ message: `User with id ${id} not found` })
   }
+  res.status(200).json(entity)
+})
+app.get('/users', (req, res) => {
+  const users = user.select()
+
+  res.status(200).json(users)
 })
 app.post('/user', async (req, res) => {
   const body = await req.toJson()
-  console.log(body)
-  res.status(201).json({ message: 'This is post:user HTTP method' })
+
+  const { name, age } = body
+  const newUser = user.insert({ name, age })
+
+  res.status(201).json(newUser)
 })
-app.put('/user', (req, res) => {
-  res.status(200).json({ message: 'This is put:user HTTP method' })
+app.put('/user/:id', async (req, res) => {
+  const body = await req.toJson()
+
+  const { name, age } = body
+  const { id } = req.params
+  const updatedUser = user.updateById(+id, { name, age })
+
+  if (!updatedUser) {
+    return res.status(404).json({ message: `User with id ${id} not found` })
+  }
+  res.status(200).json(updatedUser)
 })
-app.delete('/user', (req, res) => {
-  res.status(200).json({ message: 'This is delete:user HTTP method' })
+app.delete('/user/:id', (req, res) => {
+  const { id } = req.params
+  const deletedUser = user.deleteById(+id)
+
+  if (!deletedUser) {
+    return res.status(404).json({ message: `User with id ${id} not found` })
+  }
+  res.status(200).json(deletedUser)
 })
 
 app.listen(5000, (port) => {
